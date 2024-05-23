60b846e31fcc1a62c57ac8d09b357d29696794f3
Umm-e-Hani02 <165858620+Umm-e-Hani02@users.noreply.github.com>
2024-05-22 21:21:16 -0700

    Add files via upload

---
+++ Array.ts
@@ -0,0 +1,62 @@
+// Question 1
+// Create an array named fruits that contains the following string elements: "apple", "banana", "mango", "orange".
+let fruits= ["Apple", "Banana", "Mango", "Orange"];
+console.log(fruits);
+
+// Question 2
+// Declare an array named numbers that can contain only number elements and initialize it with the values 10, 20, 30, and 40.
+let numbers = [10, 20, 30 ,40];
+console.log(numbers);
+
+// Question 3
+// Access the third element of the fruits array and assign it to a variable named thirdFruit
+let thirdFruit = fruits;
+console.log(thirdFruit[2]);
+
+// Question 4
+// Change the second element of the numbers array to 25.
+let number = numbers
+number[1] = 25;
+console.log(number);
+
+// Question 5
+// Add the element "grape" to the end of the fruits array using the method.
+let newFruit = fruits
+fruits.push("Grape")
+console.log(newFruit);
+
+// Question 6
+// Remove the last element from the fruits array using the method and assign it to a variable named lastFruit.
+let lastFruit = fruits.pop() 
+console.log(fruits);
+
+// Question 7
+// Remove the first element from the fruits array using the method and assign it to a variable named firstFruit
+let firstFruit = fruits.shift()
+console.log(fruits);
+
+
+// Question 8
+// Add the element "kiwi" to the beginning of the fruits array using the method
+fruits.unshift("Kiwi")
+console.log(fruits);
+
+// Question 9
+// Remove 2 elements from the fruits array starting from index 1 using the method.
+fruits.shift()
+console.log(fruits);
+
+// Question 10
+// Insert the elements "pineapple" and "pear" at index 2 of the fruits array using the method
+fruits.splice(2,0, "Pineapple", "Pear")
+console.log(fruits);
+
+// Question 11
+// Create a new array named citrusFruits that contains the first two elements of the fruits array using the method
+let citrusFruits = fruits.splice(0,2)
+console.log(citrusFruits);
+
+// Question 12
+// Create a new array named lastTwoFruits that contains the last two elements of the fruits array using the method.
+let lastTwoFruits = fruits.splice(-2)
+console.log(lastTwoFruits);
